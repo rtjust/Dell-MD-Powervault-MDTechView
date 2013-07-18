a5ec6476c2220fb792067666d088570637896683
Rob Just <rtjust@gmail.com>
2013-07-18 02:08:23 -0500

    MEL Parsing\UIChange
    MEL parsed into a QTableView, still needs some work.
    UI Now expands when window is resized.
    Switched all text editors to QTextEdit for performance, QSci was very
    slow.
    Drag and drop to any part of the window, except WebView.
    File menu, non-functional.

--- MDTechView.py
+++ MDTechView.py
@@ -1,11 +1,11 @@
 import sys
 import os
 import zipfile
-from PyQt4.QtCore import QString
+import gc
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 from MDSupportBundle import MDSupportBundle
-from PyQt4.QtGui import QApplication, QMainWindow, QFontMetrics, QFont, QColor, QFileDialog, QAction, QIcon
 from PyQt4 import QtNetwork
-from PyQt4.QtCore import QUrl
 from Ui_mainwindow import Ui_MainWindow
 
 
@@ -17,12 +17,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         
         self.pushButton.clicked.connect(self.show_dialog)
 
-        self.textMEL.SendScintilla(self.textMEL.SCI_SETHSCROLLBAR, 0)
-        self.textiSCSI.SendScintilla(self.textiSCSI.SCI_SETHSCROLLBAR, 0)
-        self.textStateCap.SendScintilla(self.textStateCap.SCI_SETHSCROLLBAR, 0)
-
         self.setAcceptDrops(True)
         self.droppedFile = None
+
 
     def dragEnterEvent(self, event):
         self.droppedFile = None
@@ -35,39 +32,40 @@ class MainWindow(QMainWindow, Ui_MainWindow):
  
     def dropEvent(self, event):
         print 'Dropped file: ' + self.droppedFile # displays the file name   
+        self.extract_zipped_bundle(self.droppedFile)
 
-        if zipfile.is_zipfile(self.droppedFile):
-
+          
+    def extract_zipped_bundle(self, file_path):
+        if zipfile.is_zipfile(file_path):
             print 'is zipfile'
 
-            with zipfile.ZipFile(str(self.droppedFile)) as tempZip:
+            with zipfile.ZipFile(str(file_path)) as tempZip:
 
-                print 'Extract to: ' + str(self.droppedFile.split(os.sep)[len(self.droppedFile.split(os.sep)) - 1].split('.')[0])
+                print 'Extract to: ' + str(file_path.split(os.sep)[len(file_path.split(os.sep)) - 1].split('.')[0])
 
-                tempZip.extractall(str(self.droppedFile.split(os.sep)[len(self.droppedFile.split(os.sep)) - 1].split('.')[0]))
-                self.load_bundle(self.droppedFile.split('.')[0])
-
-            
+                tempZip.extractall(str(file_path.split(os.sep)[len(file_path.split(os.sep)) - 1].split('.')[0]))
+                self.load_bundle(file_path.split('.')[0])
 
     def show_dialog(self):
         path_dialog = QFileDialog()
-        path_dialog.setFileMode(QFileDialog.Directory)
-        bundle_path = path_dialog.getExistingDirectory(self, 'Select support bundle folder...', '')
+        path_dialog.setFileMode(QFileDialog.ExistingFile)
+        bundle_path = path_dialog.getOpenFileName(self, 'Select MD support bundle zip...', '', 'Support bundles (*.zip)')
         if bundle_path:
-            self.labelPath.setText('Current Bundle Path: ' + bundle_path)
-            self.load_bundle(bundle_path)
+            self.extract_zipped_bundle(bundle_path)
             
     
     def load_bundle(self, bundle_path):
         self.textiSCSI.clear()
         self.textSAP.clear()
-        self.textMEL.clear()
         self.textStateCap.clear()
 
         if bundle_path:
             self.labelPath.setText('Current Bundle Path: ' + bundle_path)
+            self.setWindowTitle('MD TechView Alpha - ' + bundle_path)
         else:
             self.labelPath.setText('Current Bundle Path: None')
+            self.setWindowTitle('MD TechView Alpha - None')
+
              
 
         support_bundle = MDSupportBundle(bundle_path)
@@ -82,7 +80,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.textSAP.setText(support_bundle.get_storage_array_profile())
 
         # Load MEL
-        self.textMEL.setText(support_bundle.get_major_event_log())
+        #self.textMEL.setText(support_bundle.get_major_event_log())
+        tablemodel = MyTableModel(support_bundle.parse_mel(), self)
+        self.melTable.setModel(tablemodel)
 
         # Load State Cap
         self.textStateCap.setText(support_bundle.get_state_capture_data())
@@ -90,6 +90,42 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # Load iSCSI Sessions
         self.textiSCSI.setText(support_bundle.get_iscsi_sessions())
 
+class MyTableModel(QAbstractTableModel):
+    def __init__(self, datain, parent=None, *args):
+        QAbstractTableModel.__init__(self, parent, *args)
+        self.listdata = datain
+
+    def rowCount(self, parent):
+        return len(self.listdata)
+
+    def columnCount(self, parent):
+        return 10 # hard coded, 10 items in mel that are relevant, needs to be implemented properly
+
+    def data(self, index, role):
+        if not index.isValid():
+            return QVariant()
+        elif role != Qt.DisplayRole:
+            return QVariant()
+        elif index.column() == 0:
+            return self.listdata[index.row()].date_time
+        elif index.column() == 1:
+            return self.listdata[index.row()].event_type
+        elif index.column() == 2:
+            return self.listdata[index.row()].event_category
+        elif index.column() == 3:
+            return self.listdata[index.row()].priority
+        elif index.column() == 4:
+            return self.listdata[index.row()].description
+        elif index.column() == 5:
+            return self.listdata[index.row()].event_specific_codes
+        elif index.column() == 6:
+            return self.listdata[index.row()].component_type
+        elif index.column() == 7:
+            return self.listdata[index.row()].component_location
+        elif index.column() == 8:
+            return self.listdata[index.row()].logged_by
+        else:
+            return ''
 
 
 def main():
